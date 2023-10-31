3e3eabe248e16dccb161866758dafe471586e993
Arsenty Halimovsky <ghost12@yandex.ru>
2023-10-31 03:50:47 +0300

    Create a new project

---
+++ PongGameOnC/PongGameOnC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PongGameOnC
+//
+//  Created by Арсентий Халимовский on 31.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
